f592f355b2f19b7619f9f17ffb13f4648b89442e
MisterMichi02 <63016154+MisterMichi02@users.noreply.github.com>
2020-06-27 23:53:24 +0200

    Added delete email feature

---
+++ Emails/Emails/InfoGenerator.py
@@ -0,0 +1,37 @@
+import random
+import string
+import json
+
+
+class InfoGenerator:
+    def __init__(self, firstnamesPath, lastnamesPath, randomNumberLength):
+        self.firstnames = json.load(open(firstnamesPath, "r"))
+        self.lastnames = json.load(open(lastnamesPath, "r"))
+        self.chars = string.digits + string.ascii_letters
+        self.randomNumberLength = randomNumberLength
+
+        self.format = "firstname.lastnamenumberemail"
+        self.passwordLength = 15
+
+    def SetCustomEmailFormat(self, format):
+        self.format = format
+
+    def GenerateFirstname(self):
+        return str(self.firstnames[random.randint(0, len(self.firstnames) - 1)])
+
+    def GenerateLastname(self):
+        return str(self.lastnames[random.randint(0, len(self.lastnames) - 1)])
+
+    def GenerateRandomNumber(self):
+        return "".join(random.choice(string.digits) for i in range(self.randomNumberLength))
+
+    def GenerateEmail(self, emailSuffix):
+        firstname = self.GenerateFirstname()
+        lastname = self.GenerateFirstname()
+        email = self.format.replace("firstname", str(firstname)).replace("lastname", str(lastname))
+        email = email.replace("number", self.GenerateRandomNumber())
+        email = email.replace("email", emailSuffix)
+        return str(email)
+
+    def GeneratePassword(self):
+        return "".join(random.choice(self.chars) for i in range(self.passwordLength - 1)) + "0"
---
+++ Emails/Emails/MailGenerator.py
@@ -0,0 +1,120 @@
+from Emails.InfoGenerator import InfoGenerator as ig
+from Emails.SQLManager import MySQLManager as sql
+import requests
+import json
+import time
+
+
+class MailGenerator:
+    def __init__(self):
+        self.suffix = "@dhosting.email"
+        self.sqlManager = sql("web.hak-kitz.at", "m.beihammer", "MyDatabase047", "m.beihammer")
+        self.ig = ig("firstnames.json", "lastnames.json", 5)
+
+    def CreateHostAccount(self):
+        name = "1234567890"
+        while len(name) > 9:
+            name = str(self.ig.GenerateFirstname()).lower() + str(self.ig.GenerateRandomNumber())
+        email = self.ig.GenerateEmail("@gmail.com")
+        password = self.ig.GeneratePassword()
+
+        requests.post("https://dhosting.com/registration/koszyk.html", data={
+            "_action": "dodajWyswietl", "RodzajUslugi": "glowna", "Operacja": "rejestracja",
+             "NazwaWewnetrzna": "elastic", "Promocje[0][NazwaWewnetrzna]": "domena_za_1_zl",
+             "Parametry[OkresRozliczeniowy]": "12"
+        }, cookies={
+            "hide-after-load": "yes", "PHPSESSID": "bf798196ae9e4b246602b174d37c6668"
+        })
+        response = requests.post("https://dhosting.com/registration/zamowienie2.html", data={
+            "_action": "wyslij",
+            "name": "",
+            "dPanelLogin": name,
+            "dPanelHaslo": password,
+            "email": email,
+            "zgodaDane": "T"
+        }, cookies={
+            "__cfduid": "da5234fc225d2d50368c22df423e909ad1593261478", "_fbp": "fb.1.1589747381705.1402120430",
+             "_ga": "GA1.2.1888593612.1589747381", "_gid": "GA1.2.805262201.1593261477",
+             "_hp2_id.1791626420": "{\"userId\":\"4101857338373244\",\"pageviewId\":\"1580577024557633\",\"sessionId\":\"195713887124829\",\"identity\":null,\"trackerVersion\":\"4.0\"}",
+             "_hp2_ses_props.1791626420": "{\"r\":\"https://panel.dhosting.com/poczta/v/lista/\",\"ts\":1593285737875,\"d\":\"panel.dhosting.com\",\"h\":\"/billing/v/lista/wszystkie/\"}",
+             "PHPSESSID": "bf798196ae9e4b246602b174d37c6668"
+        }).text
+
+        #print(f"Creating account: Email: {email}\nName: {name}\nPassword: {password}")
+        if json.loads(response)["Success"]:
+            self.sqlManager.insertData("dhosting_host_accounts", "(Email, Name, Password)", [email, name, password])
+            print("Account created")
+        #else:
+            #print(response)
+
+    def GetLatestHostAccount(self):
+        return self.sqlManager.getData("dhosting_host_accounts", "Email, Name, Password", "WHERE TIMESTAMPADD(Day, 12, Created) > now() order By Created desc limit 1")[0]
+
+    def GetdsID(self, account):
+        r = requests.post("https://panel.dhosting.com/", data={
+            "logowanie": "true",
+            "logowanie_username": account[1],
+            "logowanie_password": account[2]
+        })
+        return r.cookies["dsid"]
+
+    def CreateEmailAccount(self):
+        hostAccount = self.GetLatestHostAccount()
+        emailAccount = [self.ig.GenerateEmail("@dhosting.email"), self.ig.GeneratePassword()]
+        print(f"Using Email: {hostAccount[0]}\nName: {hostAccount[1]}\nPassword: {hostAccount[2]}")
+        dsid = self.GetdsID(hostAccount)
+
+        requests.post("https://panel.dhosting.com/poczta/a/dodaj-skrzynke/", data={
+            "sign_key": "nvwuaf14J6ddcuRVgJ05KJJa1x4=",
+            "adres_email": emailAccount[0],
+            "password": emailAccount[1],
+            "sms": "",
+            "wartosc_wybrana": "2",
+            "wartosc_wpisana": "2",
+            "typ":
+                [
+                    "wlasny",
+                    ""
+                 ],
+            "wyszukiwarka_aliasow": "",
+            "src_host": "",
+            "src_login": "",
+            "src_haslo": ""
+        }, cookies={
+            "dsid": dsid,
+            "login": hostAccount[1],
+        })
+        self.sqlManager.insertData("dhosting_email_accounts", "(Email, Password)", emailAccount)
+
+    def GetAllEmailAccounts(self):
+        return self.sqlManager.getData("dhosting_email_accounts", "Email, Password")
+
+    def DeleteEmailAccounts(self):
+        hostAccount = self.GetLatestHostAccount()
+        dsid = self.GetdsID(hostAccount)
+        for account in self.GetAllEmailAccounts():
+            r = requests.get(f"https://panel.dhosting.com/poczta/jv/form-usun-adres/{account[0]}/", cookies={
+                "dsid": dsid,
+                "login": hostAccount[1],
+            })
+            first = r.text.split('<input type="hidden" name="')[1].split('" value="')[0]
+            second = r.text.split(f'<input type="hidden" name="{first}" value="')[1].split('" />')[0]
+            r = requests.post("https://panel.dhosting.com/poczta/a/usun-adres//", data={
+                first: second
+            }, cookies={
+                "dsid": dsid,
+                "login": hostAccount[1],
+            })
+            self.sqlManager.deleteData("dhosting_email_accounts", f"where Email = '{account[0]}'")
+            print(f"Deleted {account[0]}")
+
+
+if __name__ == '__main__':
+    mode = "c"
+    i = MailGenerator()
+    if mode == "c":
+        for index in range(10):
+            i.CreateEmailAccount()
+            time.sleep(3)
+    elif mode == "d":
+        i.DeleteEmailAccounts()
