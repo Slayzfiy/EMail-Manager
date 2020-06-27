405c0c486322127a1d47c9af78c4b91377991c37
MisterMichi02 <63016154+MisterMichi02@users.noreply.github.com>
2020-06-27 22:15:26 +0200

    Fixed create host method

--- Emails/MailGenerator.py
+++ Emails/MailGenerator.py
@@ -13,9 +13,18 @@ class MailGenerator:
         self.ig = ig("firstnames.json", "lastnames.json", 5)
 
     def CreateHostAccount(self):
-        name = str(self.ig.GenerateFirstname()).lower() + str(self.ig.GenerateRandomNumber())
+        name = "1234567890"
+        while len(name) > 9:
+            name = str(self.ig.GenerateFirstname()).lower() + str(self.ig.GenerateRandomNumber())
         email = self.ig.GenerateEmail("@gmail.com")
         password = self.ig.GeneratePassword()
+        requests.post("https://dhosting.com/registration/koszyk.html", data={
+            "_action": "dodajWyswietl", "RodzajUslugi": "glowna", "Operacja": "rejestracja",
+             "NazwaWewnetrzna": "elastic", "Promocje[0][NazwaWewnetrzna]": "domena_za_1_zl",
+             "Parametry[OkresRozliczeniowy]": "12"
+        }, cookies={
+            "hide-after-load": "yes", "PHPSESSID": "bf798196ae9e4b246602b174d37c6668"
+        })
         response = requests.post("https://dhosting.com/registration/zamowienie2.html", data={
             "_action": "wyslij",
             "name": "",
@@ -28,12 +37,13 @@ class MailGenerator:
              "_ga": "GA1.2.1888593612.1589747381", "_gid": "GA1.2.805262201.1593261477",
              "_hp2_id.1791626420": "{\"userId\":\"4101857338373244\",\"pageviewId\":\"1580577024557633\",\"sessionId\":\"195713887124829\",\"identity\":null,\"trackerVersion\":\"4.0\"}",
              "_hp2_ses_props.1791626420": "{\"r\":\"https://panel.dhosting.com/poczta/v/lista/\",\"ts\":1593285737875,\"d\":\"panel.dhosting.com\",\"h\":\"/billing/v/lista/wszystkie/\"}",
-             "PHPSESSID": "3f2dd2beb224d1e5a4c0d5df938eb3b8"
+             "PHPSESSID": "bf798196ae9e4b246602b174d37c6668"
         }).text
 
         print(f"Creating account: Email: {email}\nName: {name}\nPassword: {password}")
-        if json.loads(response)["Success"] == "true":
+        if json.loads(response)["Success"]:
             self.sqlManager.insertData("dhosting_host_accounts", "(Email, Name, Password)", [email, name, password])
+            print("Account created")
         else:
             print(response)
 
