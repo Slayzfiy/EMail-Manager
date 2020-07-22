d80178fa9cea4503b761a84700eb1c1858e18927
MisterMichi02 <63016154+MisterMichi02@users.noreply.github.com>
2020-07-22 17:28:16 +0200

    Fixed MailChecker

--- Emails/MailChecker.py
+++ Emails/MailChecker.py
@@ -1,53 +1,45 @@
-import MySQLdb
 import imaplib
-import email as emaila
-import subprocess
+import email as e
+from email.header import decode_header
+from Emails.SQLManager import MySQLManager as sql
 
 
-def Get_Email_Password(mail):
-    db = MySQLdb.connect("web.hak-kitz.at", "m.beihammer", "MyDatabase047", "m.beihammer")
-    cursor = db.cursor()
-    cursor.execute(
-        "select Password from 1swp_email_dhosting where Email = '%s'" % mail)
-    return cursor.fetchone()[0]
+class MailChecker:
+    def __init__(self):
+        self.sqlManager = sql("web.hak-kitz.at", "m.beihammer", "MyDatabase047", "m.beihammer")
+
+    def GetMails(self, email, password):
+        mails = []
+        mail = imaplib.IMAP4_SSL("imap.dhosting.com")
+
+        try:
+            mail.login(email, password)
+        except:
+            print("Invalid Credentials")
+            return []
+
+        mail.select("inbox")
+        for ID in mail.search(None, 'ALL')[1][0].split():
+            data = mail.fetch(ID, "(RFC822)")[1]
+            email_message = e.message_from_string(data[0][1].decode("UTF-8"))
+            i = 0
+            for part in email_message.walk():
+                i += 1
+                if i == 2:
+                    body = str(part.get_payload(decode=True)).replace("\\r", "").replace("\\n", "")
+                    mails.append(body)
+        return mails
 
 
-def Get_Code(email):
-    password = Get_Email_Password(mail=email)
-    code = ""
-    # filters to filter the mails
-    sender_filter = "Epic Games <help@epicgames.com>"
-    subject_filter = 'Your Security Code'
+if __name__ == "__main__":
+    checker = MailChecker()
+    counter = 1
+    for email in checker.sqlManager.getData("dhosting_giveaway_entries", "Email"):
+        email = email[0]
+        password = checker.sqlManager.getData("dhosting_email_accounts", "Password", f"where Email ='{email}'")[0][0]
+        print(f"({counter}) Checking {email} using Password {password}")
+        mails = checker.GetMails(email, password)
+        if not mails == []:
+            print("\n" + "\n".join(mails) + "\n")
+        counter += 1
 
-    mail = imaplib.IMAP4_SSL("imap.dhosting.com")
-    mail.login(email, password)
-    mail.select("inbox")
-
-    for ID in mail.search(None, 'ALL')[1][0].split():
-        data = mail.fetch(ID, "(RFC822)")[1]
-        email_message = emaila.message_from_string(data[0][1].decode("UTF-8"))
-        if (email_message['From']) == sender_filter:
-            if (email_message['Subject']) == subject_filter:
-                i = 0
-                for part in email_message.walk():
-                    i += 1
-                    if i == 2:
-                        body = str(part.get_payload(decode=True)).replace("\\r", "").replace("\\n", "")
-                        temp = body.split(
-                            '<div style="font-family: arial,helvetica,sans-serif; mso-line-height-rule: exactly; '
-                            'color:#313131; text-align: center; font-size: 50px; letter-spacing: 20px; line-height: '
-                            '120px;">')[1]
-                        code = temp[:6]
-    return code
-
-
-def copy2clip(txt):
-    return subprocess.run('clip', universal_newlines=True, input=txt)
-
-
-while True:
-    input_mail = input('email: ')
-    input_code = Get_Code(input_mail)
-    print('Code: %s copied to clipboard!' % input_code)
-    copy2clip(input_code)
-    input('waiting..')
